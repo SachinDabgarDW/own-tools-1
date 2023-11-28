c304fd52ac16e3662ddfd054c3ec41ef535a3c56
sachin.dabgar <sachin.dabgar@dataweave.com>
2023-11-28 13:10:06 +0530

    done

--- streamlit_app.py
+++ streamlit_app.py
@@ -76,5 +76,14 @@ def main():
         converted_number = convert_number_format(input_str)
         st.markdown(f"**Converted Result:** {converted_number}")
 
+    
+    hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+    st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+
 if __name__ == "__main__":
     main()
